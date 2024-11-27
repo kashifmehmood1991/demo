f3591ac489263f6266239d126d40911d7dbc0bdf
kashifmehmood1991 <45274086+kashifmehmood1991@users.noreply.github.com>
2024-11-28 01:39:46 +0500

    containerize the java application

---
+++ src/test/java/com/example/demotask/DemotaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
